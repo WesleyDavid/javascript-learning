228e7f22c9c7e37df2597325b38f4d82792a684d
Wesley David <nonapeptide@gmail.com>
2018-10-18 14:49:51 -0700

    Playing with navigator object

--- udemy-js4beginners/ch9/functions.js
+++ udemy-js4beginners/ch9/functions.js
@@ -1,1 +1,9 @@
 // For Chapter 9
+
+function gimmeInfo() {
+ document.write(
+  "Browser name: " + navigator.appName + "<br/>" +
+  "Browser version: " + navigator.appVersion + "<br/>" +
+  "Cookies enabled: " + navigator.cookieEnabled + "<br/>" 
+  );
+} // End gimmeInfo()
