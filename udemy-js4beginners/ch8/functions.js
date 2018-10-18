efb36e530a5176f3a784b52ba08770d5aca51dbf
Wesley David <nonapeptide@gmail.com>
2018-10-17 22:23:53 -0700

    Commence the wreckage of poetry.

--- udemy-js4beginners/ch8/functions.js
+++ udemy-js4beginners/ch8/functions.js
@@ -2,3 +2,28 @@
  Some fun things to try:
   - Show the index of each occurance of a given character.
 */
+
+function wreckMyArray( theArray )
+{
+ var theChar
+
+ for (x in theArray)
+ {
+  theChar = theArray.charAt(x);
+  if ( theChar === "a" || 
+       theChar === "A" ||
+       theChar === "e" ||
+       theChar === "E" ||
+       theChar === "i" ||
+       theChar === "I" ||
+       theChar === "o" ||
+       theChar === "O" ||
+       theChar === "u" ||
+       theChar === "U") 
+  {
+   theArray[x] = "X";
+  }
+
+ return theArray;
+ }
+} // End wreckMyArray
