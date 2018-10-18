43d59deb5a4af677e973a11f5c7ea36d6be9a946
Wesley David <nonapeptide@gmail.com>
2018-10-17 23:45:11 -0700

    Now it makes more sense how to do it char at a time.

--- udemy-js4beginners/ch8/functions.js
+++ udemy-js4beginners/ch8/functions.js
@@ -8,3 +8,32 @@ function wreckMyArray( theArray )
  //I get it, this function is pointless. Leave me alone. :P
  return theArray.replace(/[aeiou]/ig,"X");
 } // End wreckMyArray
+
+
+function wreckMyArray2 ( theArray )
+{
+ //And now to do it iteratively because why not
+
+ var wreckedArray = []
+
+ for (x in theArray) {
+  theChar = theArray[x];
+
+  if ( theChar === "a" ||
+       theChar === "A" ||
+       theChar === "e" ||
+       theChar === "E" ||
+       theChar === "i" ||
+       theChar === "I" ||
+       theChar === "o" ||
+       theChar === "O" ||
+       theChar === "u" ||
+       theChar === "U" )
+  {
+   theChar = "X"
+  }
+  wreckedArray.push(theChar)
+ }
+
+ return wreckedArray;
+}
