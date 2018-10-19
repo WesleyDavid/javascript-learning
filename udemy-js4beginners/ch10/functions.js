5a854a6f024e69a165cc3fd5c68986cb7bbdacd4
Wesley David <nonapeptide@gmail.com>
2018-10-18 21:59:52 -0700

    Starting to get up in people's faces

--- udemy-js4beginners/ch10/functions.js
+++ udemy-js4beginners/ch10/functions.js
@@ -1,7 +1,13 @@
 // Chapter 10 functions
 
 function firstName() {
- alert("You're in firstName()");
+ var first = document.getElementById('firstName').value;
+
+ if (first === null || first === "")
+ {
+  alert("First name can't be empty!");
+  document.getElementById('firstError').innerHTML = "<b><-- First name can't be empty</b>";
+ }
 } // End firstName()
 
 
