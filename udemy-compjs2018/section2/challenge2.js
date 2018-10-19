d07cabd39bfd8fa919b1d5b3ece3e2bee5e5e255
Wesley David <nonapeptide@gmail.com>
2018-10-19 11:34:35 -0700

    Playing with averages.

---
+++ udemy-compjs2018/section2/challenge2.js
@@ -0,0 +1,22 @@
+var johnsTeamScores = [89,120,103];
+var mikesTeamScores = [116,94,123];
+
+var johnsTeamScoresAvg = calculateAvgScores(johnsTeamScores);
+var mikesTeamScoresAvg = calculateAvgScores(mikesTeamScores);
+
+if (johnsTeamScoresAvg < mikesTeamScoresAvg) {
+ console.log("Mike's team scores more with an average of " + mikesTeamScoresAvg);
+} else if ( johnsTeamScoresAvg === mikesTeamScoresAvg ) {
+ console.log("John and Mike's teams score an identical amount of points on avereage at " + johnsTeamScoresAvg);
+} else {
+ console.log?("John's team scores more with an average of " + johnsTeamScoresAvg);
+}
+
+
+function calculateAvgScores( scores ) {
+ var totalPoints = 0;
+ for (x in scores) {
+  totalPoints += scores[x];
+ }
+ return totalPoints / scores.length;
+}
