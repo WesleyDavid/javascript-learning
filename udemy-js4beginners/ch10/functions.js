7c96c042190c83247a36216640aeb95320aec9cb
Wesley David <nonapeptide@gmail.com>
2018-10-18 22:35:29 -0700

    I think I added them all.

--- udemy-js4beginners/ch10/functions.js
+++ udemy-js4beginners/ch10/functions.js
@@ -15,15 +15,42 @@ function firstName() {
 
 
 function lastName() {
+ var last = document.getElementById('lastName').value;
 
+ if (last === null || last === "")
+ {
+  document.getElementById('lastError').innerHTML = "<-- Last name can't be empty";
+  document.getElementById('lastName').style['background-color'] = "red";
+ } else {
+  document.getElementById('lastNameError').innerHTML = null;
+  document.getElementById('lastName').removeAttribute("style");
+ }
 } // End lastName
 
 
 function email() {
+ var email  = document.getElementById('email').value;
 
+ if (email === null || email === "")
+ {
+  document.getElementById('emailError').innerHTML = "<-- Email address can't be empty";
+  document.getElementById('email').style['background-color'] = "red";
+ } else {
+  document.getElementById('emailError').innerHTML = null;
+  document.getElementById('email').removeAttribute("style");
+ }
 } // End email
 
 
 function phone() {
+ var phone = document.getElementById('phone').value;
 
+ if (phone === null || phone === "")
+ {
+  document.getElementById('phoneError').innerHTML = "<-- Phone number can't be empty";
+  document.getElementById('phone').style['background-color'] = "red";
+ } else {
+  document.getElementById('phoneError').innerHTML = null;
+  document.getElementById('phone').removeAttribute("style");
+ }
 } // End phone
