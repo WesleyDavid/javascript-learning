2c875cfad19ada883f09abda42505e5e23091639
Wesley David <nonapeptide@gmail.com>
2018-10-19 10:50:10 -0700

    Continued switch testing.

--- udemy-compjs2018/section2/casetesting.js
+++ udemy-compjs2018/section2/casetesting.js
@@ -4,6 +4,10 @@ var age = 16;
 switch (true) {
  case age < 16:
   console.log("You cannot drive.");
+  break;
+ case age < 25:
+  console.log("You can drive, but you can't rent a car.");
+  break;
  default:
   console.log("You can drive!");
 }
@@ -11,6 +15,10 @@ switch (true) {
 switch (age) {
  case age < 16:
   console.log("You cannot drive.");
+  break;
+ case age < 25:
+  console.log("You can drive, but you can't rent a car.");
+  break;
  default:
-  console.log("You can drive!");
+  console.log("You can drive, and even rent the car to travel in!");
 }
