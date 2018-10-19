c2c41d4a21b8f16579c7fa61a9c19c6b3d2d33ff
Wesley David <nonapeptide@gmail.com>
2018-10-18 21:52:57 -0700

    Fleshing things out a bit

--- udemy-js4beginners/ch10/functions.js
+++ udemy-js4beginners/ch10/functions.js
@@ -1,1 +1,20 @@
 // Chapter 10 functions
+
+function firstName() {
+ alert("You're in firstName()");
+} // End firstName()
+
+
+function lastName() {
+
+} // End lastName
+
+
+function email() {
+
+} // End email
+
+
+function phone() {
+
+} // End phone
