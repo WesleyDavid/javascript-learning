97e8840cc888f55a504388cd73ad6db8e6f3d1fa
Wesley David <nonapeptide@gmail.com>
2018-10-19 10:58:38 -0700

    Figured it out and annotated things for future me.

--- udemy-compjs2018/section2/casetesting.js
+++ udemy-compjs2018/section2/casetesting.js
@@ -12,13 +12,14 @@ switch (true) {
   console.log("You can drive!");
 }
 
-switch (age) {
- case age < 16:
+// I get why this doesn't work now
+switch (age) { // age is an integer. For example, 16.
+ case age < 16: //This expression evaluates to the boolean false, because age is equal to 16. False is compared to 16 which is false, because they don't match.
   console.log("You cannot drive.");
   break;
- case age < 25:
+ case age < 25: // This expresion evaluates to True because age is less than 25. The boolean 'true' is compared to the integer 16 which is false.
   console.log("You can drive, but you can't rent a car.");
   break;
- default:
+ default: //Nothing else matched, so this is run:
   console.log("You can drive, and even rent the car to travel in!");
 }
