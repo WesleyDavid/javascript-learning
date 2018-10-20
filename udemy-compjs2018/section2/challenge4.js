f4345a0d48918b9367924d3f13c1a8c4fab48b75
Wesley David <nonapeptide@gmail.com>
2018-10-20 14:42:44 -0700

    Onward to challenge 4

---
+++ udemy-compjs2018/section2/challenge4.js
@@ -0,0 +1,12 @@
+var john = {
+ fullName: "John Smith",
+ mass: 72,
+ height: 1.72
+}
+
+var mark = new Object();
+mark.fullName = "Mark Richards";
+mark.mass = 68;
+mark.height = 1.68;
+
+console.log(john, mark);
