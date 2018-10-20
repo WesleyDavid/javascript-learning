6de686adfab500dba8f4afce8ce6bfb36b161d13
Wesley David <nonapeptide@gmail.com>
2018-10-20 11:50:12 -0700

    I think this should work. Need to refine it later.

---
+++ udemy-compjs2018/section2/challenge3.js
@@ -0,0 +1,34 @@
+function calculateTips ( bills ) {
+ var allTips = [];
+
+ for (var x = 0; x < bills.length; x++) {
+  if (bills[x] < 50) {
+   allTips.push(0.2);
+  } else if (bills[x] >= 50 && bills[x] < 200) {
+   allTips.push(0.15);
+  } else {
+   allTips.push(0.1)
+  }
+ } //end for
+
+ return allTips;
+} // End calculateTips()
+
+
+function calculateTotalBills ( bills, tips ) {
+ var totalBills = [];
+
+ for (x in bills) {
+  totalBills.push( bills[x] + (bills[x] * tips[x] ));
+ } // End for
+
+ return totalBills;
+}
+
+var allBills = [124, 48, 268];
+var allTips = calculateTips(allBills);
+var totalBills = calculateTotalBills( allBills, allTips );
+
+// Express the bare bills, the tip percentage, the tip amount, and the total bills.
+
+console.log(allBills, allTips, totalBills);
