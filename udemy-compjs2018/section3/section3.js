06557abe3fbc9c24b4d7b8e7e4c3e935480e8ea9
Wesley David <nonapeptide@gmail.com>
2018-10-22 09:50:04 -0700

    Adding tiers of execution context

--- udemy-compjs2018/section3/section3.js
+++ udemy-compjs2018/section3/section3.js
@@ -2,4 +2,10 @@ console.log("'this' Keyword in the Global Execution Context: " + this);
 
 function firstTier() {
  console.log("'this' Keyword in the first tier of lexical scope: " + this);
-}
+ function secondTier() {
+  console.log("'this' Keyword in the second tier of lexical scope: " + this);
+ } // End secondTier()
+ secondTier();
+} // End firstTier()
+
+firstTier();
