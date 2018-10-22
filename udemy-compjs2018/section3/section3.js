40908eb1fdc32c14d957533750ab94114fa4e24f
Wesley David <nonapeptide@gmail.com>
2018-10-22 10:18:31 -0700

    Some more object and method context frobbing.

--- udemy-compjs2018/section3/section3.js
+++ udemy-compjs2018/section3/section3.js
@@ -1,12 +1,42 @@
-console.log("'this' Keyword in the Global Execution Context: " + this);
+console.log("'this' keyword in the Global Execution Context: " + this);
 
 function firstTier() {
- console.log("'this' Keyword in the first tier of lexical scope: " + this);
+ console.log("'this' keyword in the first tier of lexical scope: " + this);
  function secondTier() {
-  console.log("'this' Keyword in the second tier of lexical scope: " + this);
+  console.log("'this' keyword in the second tier of lexical scope: " + this);
  } // End secondTier()
  secondTier();
 } // End firstTier()
 
 // firstTier();
 window.firstTier();
+
+var firstTierFuncExp = function() {
+ console.log("The 'this' keyword within a first tier function expresion: " + this);
+}
+
+firstTierFuncExp();
+
+simpleObject = {
+ simpleMethod: function() {
+  console.log("The 'this' keyword within a first tier method: " + this);
+  function simpleInnerFuncDecl() {
+   console.log("The 'this' keyword within an inner function declaration: " + this);
+  }
+
+  var simpleInnerFuncExp = function() {
+   console.log("The 'this' keyword within an inner function expression: " + this);
+  }
+
+  simpleInnerFuncDecl();
+  simpleInnerFuncExp();
+ }, // End simpleMethod
+ innerObject: {
+  innerMethod: function() {
+   console.log("The 'this' keyword within an inner object and method: " + this);
+  } // End innerMethod()
+ } // End innerObject
+}
+
+simpleObject.simpleMethod();
+simpleObject.innerObject.innerMethod();
