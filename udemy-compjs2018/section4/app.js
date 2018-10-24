b36fe293f1a3cc6ec3fed7e99e23a6466a3b2ac9
Wesley David <nonapeptide@gmail.com>
2018-10-23 21:07:40 -0700

    Next iteration of functionality.

--- udemy-compjs2018/section4/app.js
+++ udemy-compjs2018/section4/app.js
@@ -11,13 +11,33 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
-dice = 6;
 
-for (var i = 0; i < 20; i++) {
- console.log(Math.floor((Math.random() * 6) + 1));
-}
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+document.querySelector(".dice").style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function() {
+ var dice = Math.floor(Math.random() * 6) + 1;
+ var diceDOM = document.querySelector(".dice");
+ diceDOM.style.display = "block";
+ diceDOM.src = "dice-" + dice + ".png";
+ if (dice !== 1) {
+  roundScore += dice;
+  document.querySelector('#current-' + activePlayer).textContent = roundScore;
+ } else {
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+  document.querySelector('.dice').style.display = 'none';
+ }
+});
