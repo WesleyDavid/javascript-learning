1ab906f5f8801bf95eb13ed1d8136365f4f91a09
Wesley David <nonapeptide@gmail.com>
2018-10-26 14:44:23 -0700

    Playing with objects and prototypes.

--- udemy-compjs2018/section5/section5generic.js
+++ udemy-compjs2018/section5/section5generic.js
@@ -19,4 +19,26 @@ var thisFunctVarTest = new thisFunctExpTest(); // Runs the first console.log as 
 thisFunctVarTest.whatIsThis(); // Runs second console.log as [object Object]
 */
 
+// Object and prototype test
 
+var Desk = function(seats, deskLocation) {
+ this.seats = seats;
+ this.deskLocation = deskLocation;
+ this.reserved = false;
+} // End Desk object
+
+Desk.prototype.reserve = function() {
+ this.reserved = true;
+ console.log("This desk has been reserved.");
+} // End reserve desk prototype method
+
+Desk.prototype.release = function() {
+ this.reserved = false;
+ console.log("This desk is no longer reserved.");
+}
+
+var desk1, desk2, desk3;
+
+desk1 = new Desk(8, "southeast porch");
+desk2 = new Desk(10, "southeast porch");
+desk3 = new Desk(12, "southeast porch");
