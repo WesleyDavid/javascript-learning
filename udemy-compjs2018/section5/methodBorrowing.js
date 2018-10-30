d12b4b15c2e1f28ce5d07f21e8812629133746d5
Wesley David <nonapeptide@gmail.com>
2018-10-30 12:49:00 -0700

    Playing with method borrowing.

---
+++ udemy-compjs2018/section5/methodBorrowing.js
@@ -0,0 +1,15 @@
+// Make one object with a method
+
+var dice = {
+ currentRoll: 0,
+ lastRoll: 0,
+ roll: function () {
+  return "You just rolled!";
+ }
+};
+
+// Make another object without
+
+// borrow with =
+
+// Then achieve the same functionality with .call
