f5a2a6efada2207a3772fbe24c85a9dc32ada9f2
Wesley David <nonapeptide@gmail.com>
2018-10-30 14:38:13 -0700

    Finished with this round of bind experiments.

--- udemy-compjs2018/section5/methodBorrowing.js
+++ udemy-compjs2018/section5/methodBorrowing.js
@@ -1,15 +1,27 @@
 // Make one object with a method
-
 var dice = {
+ name: "varDice",
  currentRoll: 0,
  lastRoll: 0,
  roll: function () {
-  return "You just rolled!";
+  console.log("You just rolled! Also, you are: " + this.name);
  }
 };
+dice.roll();
+
 
 // Make another object without
+var sadDice = { // Sad dice cannot roll =(
+ name: "sadDice",
+ currentRoll: 0,
+ lastRoll: 0
+}
+
 
 // borrow with =
+sadDice.roll = dice.roll;
+console.log(sadDice.roll()); // The dice are now happy!
+
 
 // Then achieve the same functionality with .call
+dice.roll.call(sadDice);
